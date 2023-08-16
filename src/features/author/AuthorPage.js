e303d465e5c7981300534073a256a779858b18a9
Grzegorz Piejko <gp23.1982@gmail.com>
2023-08-16 18:36:43 +0200

    Editorial change in AuthorPage.js

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -13,9 +13,9 @@ const Author = () => (
         <p style={{ padding: "30px" }}>
           Cześć, mam na imię Grzegorz.<br/><br/> Bardzo się cieszę, że dotarłem do tego
           miejsca w kursie, chociaż im dalej w las tym coraz trudniej. Niemniej
-          nie poddaję sie i mam nadzieję, że uda mi się dopiąc swego i zostać
+          nie poddaję sie i mam nadzieję, że uda mi się dopiąć swego i zostać
           programistą ;) 
-          <br/><br/>Tymczasem cieszę się z ukończenia tego zadania, bo kolejny mały kroczek na drodze do sukcesu 😎🎈🎈
+          <br/><br/>Tymczasem cieszę się z ukończenia tego zadania, bo to kolejny mały kroczek na drodze do sukcesu 😎🎈🎈
         
         </p>
       }
